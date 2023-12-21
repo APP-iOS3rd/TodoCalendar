8f06be652d9095db3b95feadb38052e22a3c9852
funMango <138420832+funMango@users.noreply.github.com>
2023-12-21 15:14:48 +0900

    [Refactor] Category 모델 수정
    color 요소 주석처리

--- TimeBlock/Models/TodoModels/Category.swift
+++ TimeBlock/Models/TodoModels/Category.swift
@@ -1,21 +1,24 @@
-////
-////  Category.swift
-////  TimeBlock
-////
-////  Created by 이민영 on 2023/12/21.
-////
 //
-//import Foundation
-//import SwiftData
-//import SwiftUI
+//  Category.swift
+//  TimeBlock
 //
-//@Model
-//final class Category: Identifiable {
-//    var name: String
-//    var color: Color
-//    
+//  Created by 이민영 on 2023/12/21.
+//
+
+import Foundation
+import SwiftData
+import SwiftUI
+
+@Model
+final class Category: Identifiable {
+    var name: String
+    // var color: Color
+    
+    init(name: String) {
+        self.name = name
+    }
 //    init(name: String, color: Color) {
 //        self.name = name
 //        self.color = color
 //    }
-//}
+}
