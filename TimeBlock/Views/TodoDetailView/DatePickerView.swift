536d0e424ede8259ba08e33e4919ea124634e848
Minyoung Lee <mminy62@gmail.com>
2023-12-28 19:44:32 +0900

    [Refactor]: Date picker style 수정
    - Todo date 수정시 나오는 picker view 수정

--- TimeBlock/Views/TodoDetailView/DatePickerView.swift
+++ TimeBlock/Views/TodoDetailView/DatePickerView.swift
@@ -10,15 +10,15 @@ import SwiftUI
 struct DatePickerView: View {
     @Binding var date: Date
     @Binding var showDateSheet: Bool
+    
     var body: some View {
         // View로 하나 해서 밑에서부터 drawer가 나와서 조정해든지 해야됨
         VStack {
-            DatePicker(selection: $date, displayedComponents: .date) {
-                Text("날짜를 선택하세요")
-            }
-            
-            Text("Date is \(date.dateToString)")
+            DatePicker("DatePicker", selection: $date, displayedComponents: .date)
+                .datePickerStyle(.graphical)
         }
+        .presentationDetents([ .medium, .large])
+        .presentationBackground(.white)
     
     }
 }
